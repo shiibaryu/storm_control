a6ad47be3fe8b930e89a66ec8ed9b3c57ec48205
RyuseiShiiba <33246305+shiibaryu@users.noreply.github.com>
2019-03-23 01:18:34 -0700

    Add files via upload

--- broadcast_test/broadcast_receiver.c
+++ broadcast_test/broadcast_receiver.c
@@ -11,121 +11,110 @@ struct broadcast_info {
     int sd;                 
     struct sockaddr_in addr; 
 };
-typedef struct broadcast_info bc_info_t;
 
 #define MAXRECVSTRING 255 
 
 static int
-broadcast_receive(bc_info_t *info, char *errmsg)
+broadcast_receive(struct broadcast_info *info)
 {
     char recv_msg[MAXRECVSTRING+1];
     int recv_msglen = 0;
 
-    /* Receive a single datagram from the server */
     recv_msglen = recvfrom(info->sd, recv_msg, MAXRECVSTRING, 0, NULL, 0);
     if(recv_msglen < 0){
-        sprintf(errmsg, "(line:%d) %s", __LINE__, strerror(errno));
-        return(-1);
+        printf("Failed to receive message.\n");
+        return -1;
     }
 
     recv_msg[recv_msglen] = '\0';
-    printf("Received: %s\n", recv_msg);    /* Print the received string */
+    printf("Received: %s\n", recv_msg);   
 
-    return(0);
+    return 0;
 }
  
 static int
-socket_initialize(bc_info_t *info, char *errmsg)
+socket_initialize(struct broadcast_info *info)
 {
     int rc = 0;
 
     info->sd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if(info->sd < 0){
-        sprintf(errmsg, "(line:%d) %s", __LINE__, strerror(errno));
-        return(-1);
+        perror("socket");
+        return -1;
     }
 
     info->addr.sin_family = AF_INET;
     info->addr.sin_addr.s_addr = htonl(INADDR_ANY);
     info->addr.sin_port = htons(info->port);
 
-    /* ブロードキャストポートにバインドする*/
     rc = bind(info->sd, (struct sockaddr *)&(info->addr),
               sizeof(info->addr));
     if(info->sd < 0){
-        sprintf(errmsg, "(line:%d) %s", __LINE__, strerror(errno));
-        return(-1);
+        perror("bind");
+        return -1;
     }
 
-    return(0);
+    return 0;
 }
 
 static void
-socket_finalize(bc_info_t *info)
+socket_finalize(struct broadcast_info *info)
 {
-    if(info->sd != 0) close(info->sd);
+    if(info->sd != 0){
+        close(info->sd);
+    }
 
     return;
 }
 
 static int
-broadcast_receiver(bc_info_t *info, char *errmsg)
+broadcast_receiver(struct broadcast_info *info)
 {
     int rc = 0;
 
-    /* ソケットの初期化 */
-    rc = socket_initialize(info, errmsg);
-    if(rc != 0) return(-1);
-
-    /* ブロードキャストを送信する */
-    rc = broadcast_receive(info, errmsg);
-
-    /* ソケットの終期化 */
-    socket_finalize(info);
-
-    return(0);
-}
-
-/*!
- * @brief      初期化処理。
- * @param[in]  argc   コマンドライン引数の数
- * @param[in]  argv   コマンドライン引数
- * @param[out] info   ブロードキャスト情報
- * @param[out] errmsg エラーメッセージ格納先
- * @return     成功ならば0、失敗ならば-1を返す。
- */
-static int
-initialize(int argc, char *argv[], bc_info_t *info, char *errmsg)
-{
-    if(argc != 2){
-        sprintf(errmsg, "Usage: %s <port>", argv[0]);
-        return(-1);
+    rc = socket_initialize(info);
+    if(rc != 0){
+        return -1;
     }
 
-    memset(info, 0, sizeof(bc_info_t));
-    info->port       = atoi(argv[1]);
+    rc = broadcast_receive(info);
 
-    return(0);
+    socket_finalize(info);
+
+    return 0;
+}
+
+static int
+initialize(int argc, char *argv[],struct broadcast_info *info)
+{
+    if(argc != 2){
+        printf("Usage: <port>");
+        return -1;
+    }
+
+    memset(info, 0, sizeof(struct broadcast_info));
+    info->port = atoi(argv[1]);
+
+    return 0;
 }
 
 int
 main(int argc, char *argv[])
 {
     int rc = 0;
-    bc_info_t info = {0};
-    char errmsg[BUFSIZ];
+    struct broadcast_info info;
 
-    rc = initialize(argc, argv, &info, errmsg);
+    rc = initialize(argc, argv, &info);
     if(rc != 0){ 
-        fprintf(stderr, "Error: %s\n", errmsg);
-        return(-1);
+        fprintf(stderr, "Error: failed to initialize.\n");
+        return -1;
     }   
 
-    rc = broadcast_receiver(&info, errmsg);
+    rc = broadcast_receiver(&info);
     if(rc != 0){ 
-        fprintf(stderr, "Error: %s\n", errmsg);
-        return(-1);
+        fprintf(stderr, "Error: failed to broadcast_receive.\n");
+        return -1;
     }
 
-    return(0);
+    return 0;
 }
