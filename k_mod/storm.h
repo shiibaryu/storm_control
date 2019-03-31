1ff1bc31d50286da258b41e34f8afbcbf73f22e3
RyuseiShiiba <33246305+shiibaryu@users.noreply.github.com>
2019-03-31 08:21:32 -0700

    Add files via upload

---
+++ k_mod/storm.h
@@ -0,0 +1,46 @@
+/* storm.h */
+
+#ifndef _STORM_H_
+#define _STORM_H_
+
+#include <linux/netlink.h>
+
+#define STORM_VERSION "0.0.1"
+
+#ifndef __KERNEL__
+#include <netlink/genl/genl.h>
+#include <netlink/genl/family.h>
+#include <netlink/genl/ctrl.h>
+#endif
+
+#define STORM_GENL_NAME	"storm_ctl"
+#define STORM_GENL_VERSION	0x00
+
+/* genl commands */
+enum {
+	STORM_CMD_ADD_ENDPOINT,
+	__AF_GRAFT_CMD_MAX,
+};
+#define STORM_CMD_MAX	(__STORM_CMD_MAX - 1)
+
+#define CMD_SIZE 						6
+#define traffic_name_max 				15
+
+/*genl attrs*/
+struct storm_param{
+	char *dev;
+	u16  traffic_type;
+	char control_type[6];
+	int  threshold;
+	int  low_threshold;
+}__attribute__((__packed__));
+
+
+enum {
+	STORM_ATTR_NONE,
+	STORM_ATTR,	
+	__STORM_ATTR_MAX,
+};
+#define STORM_ATTR_MAX	(__STORM_ATTR_MAX - 1)
+
+#endif
