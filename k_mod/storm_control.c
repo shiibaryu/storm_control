1f0e7f3a35daf180e25e030788053422b6dcb891
RyuseiShiiba <33246305+shiibaryu@users.noreply.github.com>
2019-04-04 06:34:13 -0700

    Add files via upload

--- k_mod/storm_control.c
+++ k_mod/storm_control.c
@@ -45,6 +45,11 @@ MODULE_DESCRIPTION("This is a linux kernel module for strom control.");
 #define LEVEL				0x0004
 #define TIMER_TIMEOUT_SECS    	        1
 
+struct storm_net{
+	struct net *net;
+	struct list_head if_list;
+};
+
 struct pbl_counter{
 	int pps_counter;
 	unsigned int bps_counter;
@@ -52,6 +57,10 @@ struct pbl_counter{
 };
 
 struct storm_control_dev{
+	struct net *net;
+	struct list_head list;
+	struct rcu_head	 rcu;
+
 	struct net_device *dev;
 	struct pbl_counter *pblc;
 	int threshold;
@@ -62,9 +71,12 @@ struct storm_control_dev{
 	u16 f_flag; /*first time or not*/
 	u16 t_type; /* user specified traffic type*/
 };
-static struct storm_control_dev sc_dev;
+/*static struct storm_control_dev sc_dev;*/
 
 struct timer_list sc_timer;
+
+/* per netnamespace parameters*/
+static unsigned int storm_net_id;
 
 /*per cpu packet*/
 static DEFINE_PER_CPU(int,pc_packet);
@@ -234,7 +246,7 @@ static int route4_input(struct sk_buff *skb)
 	return 0;
 }
 
-/*the function hooks incoming packet*/
+/*the function hooked incoming packet*/
 unsigned int
 storm_hook(
 	void *priv,
@@ -369,8 +381,144 @@ storm_hook(
 	return NF_ACCEPT;
 }
 
+static struct nf_hook_ops nf_ops_storm = {
+	.hook = storm_hook,
+	.hooknum = NF_INET_PRE_ROUTING,
+        .pf = NFPROTO_IPV4,
+    	.priority = NF_IP_PRI_FIRST
+};
+
+static struct storm_control_dev *storm_find_if(struct storm_net *storm,char *dev)
+{
+	struct storm_control_dev *sc_dev;
+
+	list_for_each_entry_rcu(sc_dev,&storm->list,list){
+		if(sc_dev->dev == dev_get_by_name(&init_net,dev)){
+			return sc_dev;
+		}
+	}
+
+	return NULL;
+}
+
+static int storm_add_if(struct storm_net *storm,struct storm_param *sp)
+{
+	bool found = false;
+	struct net *sc_net;
+	struct storm_control_dev *sc_dev,*next;
+
+	sc_dev = (struct storm_control_dev *)kmalloc(sizeof(struct storm_control_dev),
+									GFP_KERNEL);
+	
+	if(!sc_dev){
+		return -ENOMEM;
+	}
+	memset(sc_dev,0,sizeof(sc_dev));
+	sc_net = get_net(&init_net);
+
+	if (IS_ERR(sc_net)) {
+		pr_debug("%s: invalid netns\n", __func__);
+		kfree(ep);
+		return PTR_ERR(ep_net);
+	}
+	sc_dev->net = sc_net;
+
+	sc_dev->dev = dev_get_by_name(&init_net,sp->dev);
+	if (!sc_dev->dev){
+		return -1;
+	}
+
+	if(sp->traffic_type & TRAFFIC_TYPE_BROADCAST){
+		sc_dev->t_type = TRAFFIC_TYPE_BROADCAST;
+		sc_dev->f_flag = FLAG_UP;
+		sc_dev->d_flag = FLAG_DOWN;
+            	printk(KERN_INFO "Control target is broadcast.\n");
+        }
+        else if(sp->traffic_type & TRAFFIC_TYPE_MULTICAST){
+		sc_dev->t_type = TRAFFIC_TYPE_MULTICAST;
+		sc_dev->f_flag = FLAG_UP;
+		sc_dev->d_flag = FLAG_DOWN;
+            	printk(KERN_INFO "Control target is multicast.\n");
+        }
+	else if(sp->traffic_type & TRAFFIC_TYPE_UNKNOWN_UNICAST){
+		sc_dev->t_type = TRAFFIC_TYPE_UNKNOWN_UNICAST;
+		sc_dev->f_flag = FLAG_UP;
+		sc_dev->d_flag = FLAG_DOWN;
+		printk(KERN_INFO "Control target is unknown_unicast.\n");
+	}
+        else{
+            printk(KERN_INFO "this traffic type could not be registered.\n");
+        }
+
+	sc_dev->pbl_type = sp->control_type;
+
+	sc_dev->threshold = sp->threshold;
+	if(sp.low_threshold > 0){
+		sc_dev->low_threshold = sp->low_threshold;
+	}	
+
+	sc_dev->reg_flag = FLAG_UP;
+
+	list_for_each_entry_rcu(next,&storm->if_list,list){
+		if(strncmp(sc_dev->dev,next->dev,sizeof(struct net_device))<0){
+			found = true;
+			break;
+		}
+	}
+
+	if(found){
+		__list_add_rcu(&sc_dev->list,next->list.prev,&next->list);
+	}
+	else{
+		list_add_tail_rcu(&sc_dev->list,&storm->if_list);
+	}
+
+	return 0;
+}
+static void storm_del_if(struct storm_control_dev *sc_dev)
+{
+	put_net(sc_dev->net);
+	dev_put(sc_dev->dev);
+	list_dev_rcu(&sc_dev->list);
+	kfree_rcu(sc_dev,rcu);
+}
+
+static __net_init int storm_init_net(struct net *net)
+{
+	struct storm_net *storm = net_generic(net,storm_net_id);
+
+	storm->net = net;
+	INIT_LIST_HEAD(&storm->if_list);
+
+	return;
+}
+
+static __net_exit void storm_exit_net(struct net *net)
+{
+	struct storm_net *storm = net_generic(net,storm_net_id);
+	struct storm_control_dev *sc_dev,*next;
+
+	rcu_read_lock();
+
+	list_for_each_entry_safe(sc_dev,next,&storm->if_list,list){
+		storm_del_if(sc_dev);
+	}
+
+	rcu_read_unlock();
+
+	return;
+}
+
+static struct pernet_operations graft_net_ops = {
+	.init = storm_init_net,
+	.exit = storm_exit_net,
+	.id   = &storm_net_id,
+	.size = sizeof(struct storm_net),
+}
+
 /* Generic Netlink implementation */
-static int storm_nl_add(struct sk_buff *skb, struct genl_info * info);
+static int storm_nl_add_if(struct sk_buff *skb, struct genl_info * info);
+static int storm_nl_del_if(struct sk_buff *skb, struct genl_info * info);
 
 static struct nla_policy storm_nl_policy[STORM_ATTR_MAX + 1] = {
 	[STORM_ATTR] = { .type = NLA_BINARY,
@@ -379,10 +527,17 @@ static struct nla_policy storm_nl_policy[STORM_ATTR_MAX + 1] = {
 
 static struct genl_ops storm_nl_ops[] = {
     {
-            .cmd	= STORM_CMD_ADD,
-	    .doit	= storm_nl_add,
-	    .policy	= storm_nl_policy,
-	    .flags	= GENL_ADMIN_PERM,
+	.cmd	= STORM_CMD_ADD_IF,
+	.doit	= storm_nl_add_if,
+	.policy	= storm_nl_policy,
+	.flags	= GENL_ADMIN_PERM,
+
+    },
+    {
+	.cmd	= STORM_CMD_DEL_IF,
+	.doit	= storm_nl_del_if,
+	.policy	= storm_nl_policy,
+	.flags	= GENL_ADMIN_PERM,
 
     },
 };
@@ -394,12 +549,16 @@ static struct genl_family storm_nl_family = {
 	.hdrsize	= 0,
 	.netnsok	= true,
 	.ops		= storm_nl_ops,
-    	.n_ops      	= ARRAY_SIZE(storm_nl_ops),
+        .n_ops      	= ARRAY_SIZE(storm_nl_ops),
 	.module		= THIS_MODULE,
 };
 
-static int storm_nl_add(struct sk_buff *skb, struct genl_info *info)
+static int storm_nl_add_if(struct sk_buff *skb, struct genl_info *info)
 {
+	int ret;
+	struct net *net = sock_net(skb->sk);
+	struct storm_net *storm = net_generic(net,storm_net_id);
+	struct storm_control_dev *sc_dev;
 	struct storm_param sp;
 
 	if (!info->attrs[STORM_ATTR]){
@@ -408,53 +567,47 @@ static int storm_nl_add(struct sk_buff *skb, struct genl_info *info)
 	}
 
 	nla_memcpy(&sp,info->attrs[STORM_ATTR],sizeof(sp));
+
+	sc_dev = storm_find_if(storm,sp.dev);
+	if(sc_dev){
+		dev_put(sc_dev->dev);
+		return -EEXIST;
+	}
+
+	ret = storm_add_if(storm,&sp);
+	if(ret < 0){
+		return ret;
+	}
+
+	return 0;
+
+}
+
+static int storm_nl_del_if(struct sk_buff *skb, struct genl_info * info)
+{
+	struct net *net = sock_net(skb->sk);
+	struct storm_net *storm = net_generic(net,graft_net_id);
+	struct storm_control_dev sc_dev;
+	struct storm_param sp;
+
+	if (!info->attrs[AF_GRAFT_ATTR_ENDPOINT]){
+		return -EINVAL;
+	}
+
+	nla_memcpy(&sp, info->attrs[STORM_ATTR_IF],
+		   sizeof(sp));
 	
-	sc_dev.dev = dev_get_by_name(&init_net,sp.dev);
-	if (!sc_dev.dev){
-		return -1;
+	sc_dev = storm_find_if(storm,sp.dev);
+	if(!ep){
+		return -ENOENT;
 	}
 
-	if(sp.traffic_type & TRAFFIC_TYPE_BROADCAST){
-		sc_dev.t_type = TRAFFIC_TYPE_BROADCAST;
-		sc_dev.f_flag = FLAG_UP;
-		sc_dev.d_flag = FLAG_DOWN;
-            	printk(KERN_INFO "Control target is broadcast.\n");
-        }
-        else if(sp.traffic_type & TRAFFIC_TYPE_MULTICAST){
-		sc_dev.t_type = TRAFFIC_TYPE_MULTICAST;
-		sc_dev.f_flag = FLAG_UP;
-		sc_dev.d_flag = FLAG_DOWN;
-            	printk(KERN_INFO "Control target is multicast.\n");
-        }
-	else if(sp.traffic_type & TRAFFIC_TYPE_UNKNOWN_UNICAST){
-		sc_dev.t_type = TRAFFIC_TYPE_UNKNOWN_UNICAST;
-		sc_dev.f_flag = FLAG_UP;
-		sc_dev.d_flag = FLAG_DOWN;
-		printk(KERN_INFO "Control target is unknown_unicast.\n");
-	}
-        else{
-            printk(KERN_INFO "this traffic type could not be registered.\n");
-        }
-
-	sc_dev.pbl_type = sp.control_type;
-
-	sc_dev.threshold = sp.threshold;
-	if(sp.low_threshold > 0){
-		sc_dev.low_threshold = sp.low_threshold;
-	}	
-
-	sc_dev.reg_flag = FLAG_UP;
+	storm_del_if(sc_dev);
 
 	return 0;
 }
 
 
-static struct nf_hook_ops nf_ops_storm = {
-	.hook = storm_hook,
-	.hooknum = NF_INET_PRE_ROUTING,
-        .pf = NFPROTO_IPV4,
-        .priority = NF_IP_PRI_FIRST
-};
     
 static int 
 __init stctl_init_module(void)
@@ -474,6 +627,11 @@ __init stctl_init_module(void)
 	sc_timer.data = 0;
 	sc_timer.function = check_packet;
 
+	ret = register_pernet_subsys(&storm_net_ops);
+	if(ret){
+		goto netns_failed;
+	}
+
 	ret = nf_register_hook(&nf_ops_storm);
         if(ret){
                 printk(KERN_INFO "failed to register hook.\n");
@@ -483,19 +641,20 @@ __init stctl_init_module(void)
 	ret = genl_register_family(&storm_nl_family);
 	if(ret){
 		printk(KERN_INFO "failed to register genl.\n");
-		goto gnel_register_failed;
+		goto genl_register_failed;
 	}
 
 	printk(KERN_INFO "storm_control module is loaded\n");
 
 	return ret;
 
-gnel_register_failed:
+genl_register_failed:
 	nf_unregister_hook(&nf_ops_storm);
 
 register_hook_failed:
 	del_timer(&sc_timer);
 
+netns_failed:
         return ret;
 }
 module_init(stctl_init_module);
@@ -509,6 +668,7 @@ __exit stctl_exit_module(void)
 	}
 	nf_unregister_hook(&nf_ops_storm);
 	genl_unregister_family(&storm_nl_family);
+	unregister_pernet_subsys(&storm_net_ops);
 	del_timer(&sc_timer);
     	printk(KERN_INFO "Storm control module was Removed.\n");
 }
