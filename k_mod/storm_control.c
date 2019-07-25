a8669fe5d675d7a401cd393b11a812b2c4f904c9
siiba <siiba@wifi-139-219.sfc.wide.ad.jp>
2019-07-25 15:31:09 +0900

    fix unknown_unicast

--- k_mod/storm_control.c
+++ k_mod/storm_control.c
@@ -496,35 +496,6 @@ static void check_packet(struct timer_list *t)
  	}
 }
 
-static struct net_bridge_fdb_entry *fdb_find_rcu(struct rhashtable *tbl,
-						 const unsigned char *addr,
-						 __u16 vid)
-{
-	struct net_bridge_fdb_key key;
-
-	WARN_ON_ONCE(!rcu_read_lock_held());
-
-	key.vlan_id = vid;
-	memcpy(key.addr.addr, addr, sizeof(key.addr.addr));
-
-	return rhashtable_lookup(tbl, &key, br_fdb_rht_params);
-}
-
-static struct net_bridge_fdb_entry *br_fdb_find(struct net_bridge *br,
-						const unsigned char *addr,
-						__u16 vid)
-{
-	struct net_bridge_fdb_entry *fdb;
-
-	lockdep_assert_held_once(&br->hash_lock);
-
-	rcu_read_lock();
-	fdb = fdb_find_rcu(&br->fdb_hash_tbl, addr, vid);
-	rcu_read_unlock();
-
-	return fdb;
-}
-
 static int find_unknown_unicast(struct sk_buff *skb){
 	struct dst_entry *dst = skb_dst(skb);
 	struct net_device *dev = dst->dev;
